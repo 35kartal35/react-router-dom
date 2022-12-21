fc08c6458e2bae8f8eacd5d6849b723c52061db6
35kartal35 <mozdemir3545@gmail.com>
2022-12-21 22:50:36 +0300

    student-body

--- my-react-project/src/components/ListStudents.js
+++ my-react-project/src/components/ListStudents.js
@@ -1,36 +1,35 @@
 import React from "react";
 
-const ListStudents = (props)=>{
-    const {students}=props
-    return(
-        <div className="container my-1">
-        <table className="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">Sıra No</th>
-      <th scope="col">Öğrenci No</th>
-      <th scope="col">Ad</th>
-      <th scope="col">Soyadı</th>
-      <th scope="col">sınıfı</th>
-      <th scope="col">okulu</th>
-    </tr>
-  </thead>
-  <tbody>
-    {students.map((student,index)=>(
-    <tr key={student.id}>
-      <th scope="row">{index+1}</th>
-      <td>{student.studentno}</td>
-      <td>{student.name}</td>
-      <td>{student.surname}</td>
-      <td>{student.studentclass}</td>
-      <td>{student.schoolname}</td>
-    </tr>
-    ))}
-  </tbody>
-</table>
-</div>
-    )
+const ListStudents = (props) => {
+  const { students } = props;
+  return (
+    <div className="container my-1">
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Sıra No</th>
+            <th scope="col">Öğr. No</th>
+            <th scope="col">Ad</th>
+            <th scope="col">Soyad</th>
+            <th scope="col">Sınıfı</th>
+            <th scope="col">Okulu</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((student,index) => (
+            <tr key={student.id}>
+              <th scope="row">{index+1}</th>
+              <td>{student.studentNo}</td>
+              <td>{student.name}</td>
+              <td>{student.surname}</td>
+              <td>{student.studentClass}</td>
+              <td>{student.schoolName}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-}
-
-export default ListStudents
+export default ListStudents;
--- my-react-project/src/pages/AddStudent.js
+++ my-react-project/src/pages/AddStudent.js
@@ -1,96 +1,108 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+
 import Header from "../components/Header";
 
-
-
 const AddStudent = () => {
-    const [studentNo,setStudentNo]=useState("")
-    const [name,setName]=useState("")
-    const [surname,setSurname]=useState("")
-    const [studentClass,setStudentClass]=useState("")
-    const [schoolName,setSchoolName]=useState("")
-    return (
-        <div>
-            <Header />
-            
-            <div className="container my-5">
-                <form >
-                <div class="mb-3">
-                    <label htmlfor="Studentno"
-                        className="form-label">Öğreni Numarası</label>
-                    <input
-                        type="number"
-                        className="form-control"
-                        id="studentno"
-                        placeholder="orn:100"
-                        value={studentNo}
-                        onChange={(event)=>setStudentNo(event.target.value)} />
-                </div>
-                <div className="container my-5">
-                    <div class="mb-3">
-                        <label htmlfor="name"
-                            className="form-label">Öğreni adı</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="name"
-                            placeholder="orn:ahmet"
-                            value={name}
-                        onChange={(event)=>setName(event.target.value)}
-                             />
-                    </div>
-                </div>
-                <div className="container my-5">
-                    <div class="mb-3">
-                        <label htmlfor="name"
-                            className="form-label">Öğreni Soyadı</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="surname"
-                            placeholder="orn:kılıç"
-                            value={surname}
-                        onChange={(event)=>setSurname(event.target.value)} />
-                    </div>
-                </div>
-                <div className="container my-5">
-                    <div class="mb-3">
-                        <label htmlfor="name"
-                            className="form-label">Sınıfı</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="studentclass"
-                            placeholder="orn:9/V"
-                            value={studentClass}
-                        onChange={(event)=>setStudentClass(event.target.value)}
-                            />
-                    </div>
-                </div>
-                <div className="container my-5">
-                    <div class="mb-3">
-                        <label htmlfor="name"
-                            className="form-label">Okulu</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="schoolname"
-                            placeholder="orn:Hoca Ahmet Yesevi Lisesi"
-                            value={schoolName}
-                        onChange={(event)=>setSchoolName(event.target.value)}
-                            />
-                    </div>
-                    <div className="d-flex justify-content-center my-5">
-                    <button type="submit" className="btn btn-outline-primary w-50">Kaydet</button>
+  const [studentNo, setStudentNo] = useState("");
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [studentClass, setStudentClass] = useState("");
+  const [schoolName, setSchoolName] = useState("");
 
-                    </div>
-                </div>
-                </form>
-            </div>
-            
-            </div>
+  const handleSave = (event) => {
+    event.preventDefault();
+    //validation
+    if (
+      studentNo === "" ||
+      name === "" ||
+      surname === "" ||
+      studentClass === "" ||
+      schoolName === ""
+    ) {
+        alert("Bütün Alanları Doldurmak Zorunludur.")
+        return
+    }
+  };
 
-            )
-}
+  return (
+    <div>
+      <Header />
+      <div className="container my-5">
+        <form onSubmit={handleSave}>
+          <div className="mb-3">
+            <label htmlFor="studentNo" className="form-label">
+              Öğrenci Numarası
+            </label>
+            <input
+              type="number"
+              className="form-control"
+              id="studentNo"
+              placeholder="Ör: 100"
+              value={studentNo}
+              onChange={(event) => setStudentNo(event.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">
+              Öğrenci Adı
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              placeholder="Ör: Ahmet"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="surname" className="form-label">
+              Öğrenci Soyadı
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="surname"
+              placeholder="Ör: Kılıç"
+              value={surname}
+              onChange={(event) => setSurname(event.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="studentClass" className="form-label">
+              Öğrenci Sınıfı
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="studentClass"
+              placeholder="Ör: 5/B"
+              value={studentClass}
+              onChange={(event) => setStudentClass(event.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="schoolName" className="form-label">
+              Okul Adı
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="schoolName"
+              placeholder="Ör: Cumhuriyet İ.Ö.O."
+              value={schoolName}
+              onChange={(event) => setSchoolName(event.target.value)}
+            />
+          </div>
+          <div className="d-flex justify-content-center my-5">
+            <button type="submit" className="btn btn-outline-primary w-50">
+              Kaydet
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-            export default AddStudent
+export default AddStudent;
